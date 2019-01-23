ddfec1568be1d0faf1c0c006e881f71ae7ba5e33
chaochao.jia <chaochao.jia@56qq.com>
2019-01-23 21:00:42 +0800

    feat: 增加 index 页面

--- build/index.js
+++ build/index.js
@@ -1,13 +1,40 @@
 const fs = require('fs');
 const cp = require('child_process');
+const path = require('path');
+const ejs = require('ejs');
+const mkdirp = require('mkdirp');
 
-files = fs.readdirSync('src')
+const srcPath = 'src';
+const distPath = 'dist';
 
-files.map((file)=> {
-  const fileReg = /(?!.*\\).*\.mdx/g; // 文件名，带后缀
-  const fileNameReg = /(?!.*\\).*(?=\.mdx)/g; // 文件名，不带后缀
-  const fileFullName = file.match(fileReg)[0];
-  const fileName = file.match(fileNameReg)[0];
-  // 构建文件
-  cp.exec(`mdx-deck build src/${fileFullName} -d dist/${fileName}/`);
-});
+const fileReg = /(?!.*\\).*\.mdx$/; // 文件名，带后缀
+const fileNameReg = /(?!.*\\).*(?=\.mdx$)/; // 文件名，不带后缀
+
+// 遍历 src 文件
+const files = fs.readdirSync(srcPath);
+const slideNames = files.reduce((acc, file) => {
+  const fileFullName = (file.match(fileReg) || [])[0];
+  const fileName = (file.match(fileNameReg) || [])[0];
+
+  if (fileFullName && fileName) {
+    acc.push(fileName);
+    console.log(file, fileFullName, fileName, acc);
+    // 构建 slides 文件
+    cp.exec(`mdx-deck build ${srcPath}/${fileFullName} -d ${distPath}/${fileName}/`);
+  }
+  return acc;
+}, []);
+
+// 构建主页面
+const indexFile = fs.readFileSync(path.join(__dirname, '../src/index.ejs'), 'utf-8');
+const data = {
+  slideNames
+};
+const options = {};
+
+const html = ejs.compile(indexFile)(data, options);
+
+const outDir = path.join(__dirname, '../dist');
+if (!fs.existsSync(outDir)) mkdirp.sync(outDir);
+
+fs.writeFileSync(`${outDir}/index.html`, html);
